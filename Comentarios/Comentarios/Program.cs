bbabc24212f45f033b853149a0af191db232cfbb
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-04-15 20:03:34 -0300

    Comentários

---
+++ Comentarios/Comentarios/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comentarios
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Comentarios
+
+            // Este comando mostra a mensagem
+
+            /*
+             * texto de teste
+             * para testar
+             * comentarios
+             */
+
+            #endregion
+
+            #region Primeira parte
+
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Ola Mundo!");
+
+            #endregion
+
+            #region Segunda parte
+
+
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Ola Mundo!");
+            Console.WriteLine("Ola Mundo!");
+            Console.ReadKey();
+
+            #endregion
+        }
+    }
+}
