db8e5ab5c4e522773380cb884d9775449bd1c888
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-04-15 21:57:09 -0300

    Comandos de entrada e saída

---
+++ EntradaSaida/EntradaSaida/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntradaSaida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("a"); // imprime o argumento
+            //Console.WriteLine("b"); // com quebra de linha
+
+            //Console.Write("c "); // imprime
+            //Console.Write("d"); // sem quebra de linha
+
+            //int letraCod = Console.Read(); // lê o código do caractere
+
+            //Console.WriteLine(letraCod);
+
+            //Console.ReadKey(true); // retorna a tecla digitada
+            //Console.ReadKey();
+
+            string texto;
+
+            texto = Console.ReadLine(); // lê a string
+            Console.WriteLine(texto);
+
+            Console.ReadKey();
+
+        }
+    }
+}
