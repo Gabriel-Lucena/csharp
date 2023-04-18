bf6cac78585de51fd48330f77b1dc91992f8571c
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-04-18 04:38:18 -0300

    Estruturas de Controle

---
+++ Controle/Controle/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Switch - Case
+
+            int escolha = 1;
+
+            switch (escolha)
+            {
+                default:
+                    Console.WriteLine("Opção padrão");
+
+                    break;
+
+                case 1:
+
+                    Console.WriteLine("Opção 1");
+
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Opção 2");
+
+                    break;
+
+                case 3:
+
+                case 4:
+
+                    Console.WriteLine("Opção combinada");
+
+                    break;
+            }
+
+            #endregion
+
+            #region goto
+
+            Inicio:
+
+            Console.Write("Escolha uma opção: ");
+            int op = int.Parse(Console.ReadLine());
+            int valor = 0;
+
+
+            switch (op)
+            {
+                default:
+                    goto Inicio;
+                case 1:
+                    valor += 100;
+                    break;
+                case 2:
+                    valor += 50;
+                    goto case 1;
+            
+            }
+
+
+            Console.WriteLine("Valor final: " + valor);
+
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+}
