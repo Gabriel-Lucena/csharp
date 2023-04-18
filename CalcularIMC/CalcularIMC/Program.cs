8f30a977e4ad44268f13a3eecd1f688b69103dc1
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-04-18 04:09:14 -0300

    Calculadora de IMC

---
+++ CalcularIMC/CalcularIMC/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CalcularIMC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double altura, massa, imc;
+
+            Console.Write("Digite sua altura em metros: ");
+            altura = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite sua massa em quilos: ");
+            massa = double.Parse(Console.ReadLine());
+
+            imc = massa / (altura * altura);
+
+            if (imc < 20)
+            {
+                Console.WriteLine("\nIMC = " + imc + " -> Abaixo do peso");
+            } 
+                else if (imc <= 24)
+            {
+
+                Console.WriteLine("\nIMC = " + imc + " -> Normal");
+
+            } else if (imc <= 29)
+            {
+
+                Console.WriteLine("\nIMC = " + imc + " -> Acima do peso");
+            } else if (imc <= 34)
+            {
+                Console.WriteLine("\nIMC = " + imc + " -> Obesidade");
+            } else
+            {
+                Console.WriteLine("\n IMC = " + imc + " -> Obesidade II");
+            }
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
