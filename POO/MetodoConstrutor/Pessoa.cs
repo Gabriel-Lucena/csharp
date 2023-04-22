b22a6b5877395e5a73f1f60ea264c90f9f46fc16
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-04-22 02:12:51 -0300

    Método Construtor

---
+++ POO/MetodoConstrutor/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetodoConstrutor
+{
+    class Pessoa
+    {
+        public string nome;
+        public string sobrenome;
+        public int anoNascimento;
+        public int idade;
+
+        public Pessoa()
+        {
+            nome = "Desconhecido";
+            sobrenome = "Nenhum";
+            anoNascimento = 0;
+            idade = 0;
+        }
+        public Pessoa(string nome, string sobrenome, int anoNascimento)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.anoNascimento = anoNascimento;
+            idade = CalcularIdade();
+        }
+        public Pessoa(string nome, string sobrenome)
+        {
+            this.nome = nome;
+            this.sobrenome = sobrenome;
+            this.anoNascimento = 2005;
+            idade = CalcularIdade();
+        }
+        private int CalcularIdade()
+        {
+            return 2023 - anoNascimento;
+        }
+    }
+}
---
+++ POO/MetodoConstrutor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MetodoConstrutor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa();
+
+            Console.WriteLine(pessoa.nome);
+            Console.WriteLine(pessoa.sobrenome);
+            Console.WriteLine(pessoa.anoNascimento);
+            Console.WriteLine(pessoa.idade);
+
+            Console.WriteLine();
+
+            Pessoa pessoa2 = new Pessoa("asdasasd", "fgfgfgfgfg", 12);
+            Console.WriteLine(pessoa2.nome);
+            Console.WriteLine(pessoa2.sobrenome);
+            Console.WriteLine(pessoa2.anoNascimento);
+            Console.WriteLine(pessoa2.idade);
+
+            Console.WriteLine();
+
+            Pessoa pessoa3 = new Pessoa("231231asd", "asdaf34");
+            Console.WriteLine(pessoa3.nome);
+            Console.WriteLine(pessoa3.sobrenome);
+            Console.WriteLine(pessoa3.anoNascimento);
+            Console.WriteLine(pessoa3.idade);
+
+            Console.ReadKey();
+        }
+    }
+}
