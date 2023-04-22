2c963e091199273da89708b816725406782e8df9
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-04-21 21:38:57 -0300

    Sequência de Fibonacci

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valor,ant2 = 0, ant1 = 1, aux, aux2, quantidade;
+
+            Console.Write("Digite a quantidade de números da sequência: ");
+            quantidade =  int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 0; i < quantidade; i++)
+            {
+                    valor = ant2 + ant1;
+                if (i == 0)
+                {
+                    Console.Write(ant2 + ", ");
+                }
+                else if (i == 1) 
+                {
+                    Console.Write(ant1 + ", ");
+                }
+                else
+                {
+                    aux2 = ant1;
+                    aux = valor;
+                    valor = ant2 + ant1;
+                    ant1 = aux;
+                    ant2 = aux2;
+                    
+
+                    if ( quantidade - 1 == i)
+                    {
+                        Console.Write(valor);
+                    } else
+                    {
+                        Console.Write(valor + ", ");
+                    }
+                    
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
