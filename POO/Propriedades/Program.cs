b9920ac51acf3c256a86cf098a9f17cc5fc26e3f
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-04-23 23:07:04 -0300

    Private e Public - Propriedades

---
+++ POO/Propriedades/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Propriedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teste teste = new Teste();
+
+            teste.Nome = "A";
+            teste.Idade = 123;
+
+            Console.WriteLine(teste.Nome + " " + teste.Sobrenome);
+            Console.WriteLine(teste.Idade);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ POO/Propriedades/Teste.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Propriedades
+{
+    class Teste
+    {
+        // Definição de um campo
+        private string _nome;
+        public string Sobrenome { get; } = "ABCD";
+        private int _idade;
+
+        //Definição de uma propriedade
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+
+            set
+            {
+                _nome = value;
+            }
+        }
+
+        public int Idade
+        {
+            get
+            {
+                return _idade;
+            }
+
+            set
+            {
+                if (value < 18)
+                {
+                    Console.WriteLine("Idade não pode ser inferior a 18 anos.");
+                }
+                else
+                {
+                    _idade = value;
+                }
+            }
+        }
+
+
+
+        public void Apresentar()
+        {
+            if (_nome != "")
+                Console.WriteLine("Olá, " + _nome);
+        }
+    }
+}
