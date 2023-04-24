0b6ab1166bd19e9215872a4bf3a3821a668e5a97
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-04-23 21:17:55 -0300

    Série

---
+++ Harmonico/Harmonico/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harmonico
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+            int contador;
+
+            Console.Write("Até qual número: ");
+            contador = int.Parse(Console.ReadLine());
+
+            for (double i = 1; i < contador; i++)
+            {
+                soma += 1 / i;
+            }
+
+            Console.WriteLine(soma);
+
+            Console.ReadKey();
+        }
+    }
+}
