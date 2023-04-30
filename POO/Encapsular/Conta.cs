d36d3ed877c2ab12bb860ffb40290f6d8eb061fa
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-04-30 20:00:09 -0300

    Encapsulamento

---
+++ POO/Encapsular/Conta.cs
@@ -0,0 +1,49 @@
+﻿namespace Encapsular
+{
+    class Conta
+    {
+        private string _cliente;
+        private double _saldo;
+        private double _taxa = 10;
+
+        public string Cliente
+        {
+            get { return _cliente; }
+            set
+            {
+                if (value != "Nome" && value != "Sobrenome")
+                {
+                    _cliente = "Visitante";
+                    Saldo = 0;
+                }
+                else
+                {
+                    _cliente = value;
+                    Saldo = 100;
+                }
+            }
+        }
+
+        public double Saldo
+        {
+            get{
+                return _saldo;
+            }
+            private set
+            {
+                _saldo = value;
+            }
+        }
+
+        public void Sacar(double valor)
+        {
+            valor += _taxa;
+            _saldo -= valor;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+    }
+}
---
+++ POO/Encapsular/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+
+            conta.Cliente = "Nome";
+            //conta.Saldo = 100;
+
+            // Operação de Depósito
+
+            double valor = 100;
+            conta.Depositar(valor);
+
+            // Operação de Saque
+
+            double valor2 = 50;
+            conta.Sacar(valor2);
+
+            //Resultado Saldo Atual
+            Console.WriteLine("Cliente: " + conta.Cliente);
+            Console.WriteLine("Saldo: " + conta.Saldo);
+
+            Console.ReadKey();
+        }
+    }
+}
