15fd5d4a5ef45e765259dc23419cdceca4edcdc4
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-05-04 23:28:52 -0300

    Modificadores de Acesso

---
+++ POO/Modificadores/Classes.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+
+namespace Modificadores
+{
+    class Teste
+    {
+        /*
+         * private - membros da classe são privados, só são visíveis dentro da classe
+         * protected - objetos não tem acesso
+         *             , mas membros das classes herdadas podem ter acesso
+         * internal - tem visibilidade em todo assembly, não é visível quando a classe for importada
+         */
+
+        private string nome;
+        public string sobrenome;
+
+        private void Metodo1() { }
+        public void Executar() { }
+
+    }
+
+    class Humano
+    {
+        /*
+         * A classe que herda pode interagir com nome
+         */
+
+        protected string nome;
+        private string sobrenome;
+        internal int idade;
+    }
+
+    class Homem : Humano
+    {
+        public void Metodo()
+        {
+            nome = "";
+            idade = 18;
+        }
+    }
+}
---
+++ POO/Modificadores/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Teste teste = new Teste();
+
+            teste.sobrenome = "";
+            //teste.nome = "";
+            //teste.Metodo1();
+            teste.Executar();
+
+            Homem homem = new Homem();
+
+            homem.Metodo();
+            homem.idade = 19;
+
+            Console.ReadKey();
+        }
+    }
+}
