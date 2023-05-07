b2781cf32c23d9ca41cd674ca3f3b5d0a9607e49
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-05-07 18:31:36 -0300

    Interfaces

---
+++ POO/Polimorfismo/Conta.cs
@@ -0,0 +1,38 @@
+﻿namespace Polimorfismo
+{
+    class CaixaEletronico : IConta
+    {
+        public string Usuario { get ; set ; }
+
+        public void Depositar()
+        {
+            
+        }
+
+        public void Sacar()
+        {
+           
+        }
+
+        public void Saldo()
+        {
+          
+        }
+
+        public void SolicitarEmprestimo()
+        {
+
+        }
+    }
+
+    interface IConta // Membros abstratos
+    {
+        string Usuario { get; set; }
+
+        void Depositar();
+
+        void Sacar();
+
+        void Saldo();
+    }
+}
