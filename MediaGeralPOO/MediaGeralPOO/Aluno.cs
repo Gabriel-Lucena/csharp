2ec0bbf9ea35ad64758dd3e1b330b0916221525e
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-05-08 01:32:58 -0300

    Média Geral POO

---
+++ MediaGeralPOO/MediaGeralPOO/Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MediaGeralPOO
+{
+    internal class Aluno
+    {
+        public string nome { get; private set; }
+
+        private double[] _notas;
+
+        public double Media
+        {
+            get
+            {
+                return CalcularMedia();
+            }
+        }
+
+        public Aluno(string nome, int provas)
+        {
+            this.nome = nome;
+            _notas = new double[provas];
+
+        }
+
+        public void InserirNotas()
+        {
+            for (int i = 0; i < _notas.Length; i++)
+            {
+                Console.Write("Nota #" + (i + 1) + ": ");
+                _notas[i] = double.Parse(Console.ReadLine());
+            }
+        }
+
+        private double CalcularMedia()
+        {
+            double somaNotas = 0;
+
+            for (int i = 0; i < _notas.Length; i++)
+            {
+                somaNotas += _notas[i];
+            }
+
+            double media = somaNotas / _notas.Length;
+
+            return media;
+        }
+    }
+}
---
+++ MediaGeralPOO/MediaGeralPOO/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaGeralPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "### Média Geral dos Alunos ###";
+
+            Console.WriteLine("Quantos alunos? ");
+            int quantidadeAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Aluno[] vetorAlunos = new Aluno[quantidadeAlunos];
+
+            for (int i = 0; i < vetorAlunos.Length; i++)
+            {
+                Console.Clear();
+
+                Console.Write("Aluno #" + (i + 1) + " Nome..: ");
+                string nomeAluno = Console.ReadLine();
+
+                Console.Write("Aluno #" + (i + 1) + " Provas: ");
+                int quantidadeProvas = int.Parse(Console.ReadLine());
+
+                vetorAlunos[i] = new Aluno(nomeAluno, quantidadeProvas);
+
+                Console.WriteLine("Insira as notas do aluno " + nomeAluno);
+                vetorAlunos[i].InserirNotas();
+            }
+
+            Console.Clear();
+
+            double somaGeral = 0;
+
+            foreach (Aluno aluno in vetorAlunos)
+            {
+                Console.WriteLine("Aluno: " + aluno.nome);
+                Console.WriteLine("Media: " + aluno.Media);
+
+                Console.WriteLine();
+
+                somaGeral += aluno.Media;
+            }
+
+            double mediaGeral = somaGeral / vetorAlunos.Length;
+
+            Console.WriteLine("Média Geral dos Alunos: " + Math.Round(mediaGeral, 1));
+
+            Console.ReadKey();
+        }
+    }
+}
